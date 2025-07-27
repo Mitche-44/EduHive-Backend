fb61082e3e2e60ee216cdbde49219badbced5684
Kennedy Ouma Odero <kennedyrogouma222@gmail.com>
2025-07-27 23:03:36 +0300

    feat: import learner-activity, progress and path models

--- models/__init__.py
+++ models/__init__.py
@@ -1,1 +1,18 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+# Import all models explicitly
 from .user import User
+from .learner_activity import LearnerActivity
+from .progress import Progress
+from .path import Path
+# from .module import Module
+# from .quiz import Quiz
+# from .quiz_attempt import QuizAttempt
+# from .subscription import Subscription
+# from .badge import Badge
+# from .chat_message import ChatMessage
+# from .comment import Comment
+# from .community_post import CommunityPost
+# from .approval_request import ApprovalRequest
