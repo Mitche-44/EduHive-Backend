f308021b4a145b018a4e683a331af698ac78f58f
Kennedy Ouma Odero <kennedyrogouma222@gmail.com>
2025-07-30 00:27:43 +0300

    Register subscription routes to Flask app

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
 from flask import Flask, jsonify
 from config import Config
-from extensions import db, migrate, bcrypt, cors
+from extensions import db, migrate, bcrypt, cors, mail, socketio
 from flask_jwt_extended import JWTManager
+from flask_socketio import SocketIO
 from resources import api_bp
 from models import leaderboard
 from models import newsletter   
 from models import subscription
 from models import testimonial
+from resources.admin.testimonial_admin import admin_testimonial_bp
+from resources.admin.subscriptions import admin_subscriptions_bp
+from resources.admin.admin_leaderboard import admin_leaderboard_bp
+
+
 
 
 jwt = JWTManager()
@@ -26,6 +32,7 @@ def create_app():
     migrate.init_app(app, db)
     bcrypt.init_app(app)
     cors.init_app(app, resources={r"/api/*": {"origins": "*"}}, supports_credentials=True)
+    socketio.init_app(app)
 
 
 
@@ -34,11 +41,16 @@ def create_app():
     # Register blueprints here later
     # from resources.customers import customers_bp
     # app.register_blueprint(customers_bp, url_prefix="/api/customers")
+    
+    # ✅ Add root route here
+    @app.route("/")
+    def index():
+        return jsonify({"message": "EduHive API is running"}), 200
 
     return app
 
 if __name__ == "__main__":
     app = create_app()
-    app.run(debug=True)    
+    socketio.run(app, host="0.0.0.0", port=5000, debug=True)  
     
     
